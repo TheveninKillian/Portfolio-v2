d7c3c116a94498fe4d0b1a551fe2ad564c454b56
Killian <thevenin.killian@gmail.com>
2021-07-28 15:51:17 +0200

    add chat online

--- src/main.ts
+++ src/main.ts
@@ -10,4 +10,19 @@ const routes = setupLayouts(generatedRoutes)
 
 export const createApp = ViteSSG(App, { routes }, (ctx) => {
   Object.values(import.meta.globEager('./modules/*.ts')).map(i => i.install?.(ctx))
+
+  const BASE_URL = 'https://app.chatwoot.com'
+  const g = document.createElement('script')
+  const s = document.getElementsByTagName('script')[0]
+
+  g.src = `${BASE_URL}/packs/js/sdk.js`
+  if (s.parentNode !== null)
+    s.parentNode.insertBefore(g, s)
+
+  g.onload = function() {
+    window.chatwootSDK.run({
+      websiteToken: 'cK8yfMkTjCCQycrhvPXXXC7d',
+      baseUrl: BASE_URL,
+    })
+  }
 })
