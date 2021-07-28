9aa4b0ac68f2e66b59d6dd55df470a28f9e77239
Killian <thevenin.killian@gmail.com>
2021-07-28 15:15:01 +0200

    add condition window

--- src/device.ts
+++ src/device.ts
@@ -5,8 +5,10 @@ const width = ref(0)
 const height = ref(0)
 
 export const recoverDevice = () => {
-  width.value = window.innerWidth
-  height.value = window.innerHeight
+  if (typeof window !== 'undefined') {
+    width.value = window.innerWidth
+    height.value = window.innerHeight
+  }
 
   if (width.value < 600) device.value = 'mobile'
   else if (width.value >= 600 && height.value > width.value) device.value = 'tablet'
--- src/resize.ts
+++ src/resize.ts
@@ -3,7 +3,8 @@ import { ref } from 'vue'
 const width = ref(0)
 
 export const handleResize = () => {
-  width.value = window.innerWidth
+  if (typeof window !== 'undefined')
+    width.value = window.innerWidth
 
   return width.value
 }
