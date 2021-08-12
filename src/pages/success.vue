6c1d5ae7ed8b42b619a62341e441cee89c063913
Killian <thevenin.killian@gmail.com>
2021-08-13 01:44:56 +0200

    create page success form

---
+++ src/pages/success.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+
+<template>
+  <section>
+    <div class="success">
+      <p text="size-25px" font="bold">
+        Thank you!
+      </p>
+
+      <p>Your form submission has been received.</p>
+
+      <div>
+        <button
+          class="btn-special mt-8"
+          @click="router.back()"
+        >
+          Back to our site
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+@import 'variables.scss';
+
+.success{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+
+  transform: translate(-50% -50%);
+
+  background-color: $color-black;
+}
+</style>
