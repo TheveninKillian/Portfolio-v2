c86b9dfcf2882ab6cb9e2d30f70f90111917deaf
Killian <thevenin.killian@gmail.com>
2021-08-13 01:44:40 +0200

    create page 404

---
+++ src/layouts/404.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+
+<template>
+  <section class="px-4 py-10 text-center">
+    <div>
+      <p class="text-4xl">
+        <carbon-warning class="inline-block" />
+      </p>
+    </div>
+
+    <router-view />
+
+    <div>
+      <button
+        class="btn-special mt-8"
+        @click="router.back()"
+      >
+        Back
+      </button>
+    </div>
+  </section>
+</template>
